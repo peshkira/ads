0241b83aba1fb99a18f05e72e26466072b21ff05
Filip Hianik <nonsense86@gmail.com>
2011-06-10 11:47:22 +0200

    server config saved in property file, server connects to spread and joins the servergroup

--- ads_server/src/at/tuwien/ads11/ReplicatedServer.java
+++ ads_server/src/at/tuwien/ads11/ReplicatedServer.java
@@ -1,5 +1,10 @@
 package at.tuwien.ads11;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.rmi.NoSuchObjectException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -8,7 +13,12 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Properties;
 import java.util.Set;
+
+import spread.SpreadConnection;
+import spread.SpreadException;
+import spread.SpreadGroup;
 
 import at.tuwien.ads11.common.ClientMock;
 import at.tuwien.ads11.common.Constants;
@@ -26,22 +36,46 @@ public class ReplicatedServer implements IServer {
     private Set<ClientMock> clients;
 
     private IServer proxy;
+    
+    private int rmiPort;
+    private int daemonPort;
+    private String daemonIP;
+    private String serverId;
+    
+    private SpreadConnection spreadCon;
+    private SpreadGroup serverGroup;
 
-    public ReplicatedServer(int port) {
-       this.startRMIRegistry(port);
-       this.games = new ArrayList<Game>();
-       this.clients = new HashSet<ClientMock>();
+    public ReplicatedServer(Properties props) {
+    	this.serverId = props.getProperty("serverId");
+    	this.rmiPort = Integer.parseInt(props.getProperty("rmiPort"));
+    	this.daemonPort = Integer.parseInt(props.getProperty("daemonPort"));
+    	this.daemonIP = props.getProperty("daemonIP");
+    	this.games = new ArrayList<Game>();
+    	this.clients = new HashSet<ClientMock>();
        
     }
 
     public static void main(String args[]) {
 
         if (args == null || args.length != 1) {
-            System.exit(1);
+        	System.out.println("Invalid argument count - provide name of the config file.");
+        	System.exit(1);
         }
 
-        int port = Integer.parseInt(args[0]);
-        ReplicatedServer server = new ReplicatedServer(port);
+        Properties props = new Properties();
+        try {
+			props.load(new FileInputStream(args[0]));
+		} catch (FileNotFoundException e) {
+			System.out.println("Config file: " + args[0] + " not found.");
+			e.printStackTrace();
+			System.exit(1);
+		} catch (IOException e) {
+			System.out.println("Error in processing the config file.");
+			System.exit(1);
+		}
+		
+        ReplicatedServer server = new ReplicatedServer(props);
+        server.start();
         Thread console = new Thread(new ServerConsole(server));
         console.start();
 
@@ -97,22 +131,46 @@ public class ReplicatedServer implements IServer {
 
     protected void shutdown() {
         try {
-            UnicastRemoteObject.unexportObject(this.proxy, true);
+            serverGroup.leave();
+        	UnicastRemoteObject.unexportObject(this.proxy, true);
             UnicastRemoteObject.unexportObject(this.registry, true);
             
         } catch (NoSuchObjectException e) {
             e.printStackTrace();
-        }
+        } catch (SpreadException e) {
+        	System.err.println("Error while leaving the server group.");
+			e.printStackTrace();
+		}
 
     }
 
     // ========= private ===========
 
-    private void init() {
+    private void start() {
         // connect to the spread deamon
         // check if this is the first server in the group
         // if yes create the proxy and bind it to a registry...
         // if no get the proxy, add this server to it and rebind it...
+    	connectToSpread();
+    	startRMIRegistry(rmiPort);
+    }
+    
+    private void connectToSpread() {
+    	spreadCon = new SpreadConnection();
+    	serverGroup = new SpreadGroup();
+    	
+    	try {
+			spreadCon.connect(InetAddress.getByName(daemonIP), daemonPort, serverId, false, true);
+			serverGroup.join(spreadCon, Constants.SPREAD_SERVER_GROUP);
+		} catch (UnknownHostException e) {
+			System.err.println("Can not find daemon: " + daemonIP);
+			e.printStackTrace();
+			System.exit(1);
+		} catch (SpreadException e) {
+			System.err.println("Error while connecting to Spread.");
+			e.printStackTrace();
+			System.exit(1);
+		}
     }
 
     private void startRMIRegistry(int port) {
